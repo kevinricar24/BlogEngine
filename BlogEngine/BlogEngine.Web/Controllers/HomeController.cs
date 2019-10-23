c33ff723b1a56a654c307c125829396409e4b1de
kevin ricardo sejin <kevinricar24@hotmail.com>
2019-10-23 18:39:33 -0500

    update comment section and controller

--- BlogEngine/BlogEngine.Web/Controllers/HomeController.cs
+++ BlogEngine/BlogEngine.Web/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BlogEngine.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using BlogEngine.Web.Models;
+using System.Diagnostics;
 
 namespace BlogEngine.Web.Controllers
 {
